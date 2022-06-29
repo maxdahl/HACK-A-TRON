a1e816ef94ca4f6b3bee09ef6354efbc7f7d6f11
Max Dahl <github@daagur.de>
2022-06-29 12:49:01 +0200

    initial commit

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+
+
+import "./App.css";
+
+function App() {
+  return (
+    <RecoilRoot>
+      <Suspense>
+        <Router>
+          <Routes>
+            <Route path="/" element={<div>DASHBOARD</div>} />
+          </Routes>
+        </Router>
+      </Suspense>
+    </RecoilRoot>
+  );
+}
+
+export default App;
