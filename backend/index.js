089267630bae1a97d087dd7ee2dd88adaa44ca23
Max Dahl <github@daagur.de>
2022-06-29 15:19:51 +0200

    added backend setup

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
