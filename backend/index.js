0b18178943a5f9159c41f5490beca50852d1564e
Max Dahl <github@daagur.de>
2022-06-29 15:19:28 +0200

    added backend setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-restricted-syntax  */
+
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+async function startServer() {
+  const port = process.env.APP_PORT || 5000;
+  app.listen(port, () => {
+    console.info(`Server started on port ${port}`);
+  });
+}
+
+startServer();
