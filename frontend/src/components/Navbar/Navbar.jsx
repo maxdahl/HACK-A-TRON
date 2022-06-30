68936f68af9c1a597f5d0d15da8d9d5082a377e2
Moses Levin <moses.levin14@gmail.com>
2022-06-30 15:55:05 +0200

    navbar cool cool cool

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -20,16 +20,26 @@
 
 // export default Navbar;
 
+import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
-/* import { Link } from "react-router-dom"; */
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
+const boxSX = {
+  "&:hover": {
+    border: "1px solid #E77620",
+    color: "black",
+    backgroundColor: "#E89759",
+  },
+};
+
 const StyledFab = styled(Fab)({
+  backgroundColor: "#E77620",
   position: "absolute",
   zIndex: 1,
   top: -30,
@@ -42,24 +52,41 @@ export default function Navbar() {
   return (
     <AppBar
       position="fixed"
-      color="secondary"
-      sx={{ top: "auto", bottom: 0, backgroundColor: "#183650" }}
+      /*  color="secondary" */
+      sx={{ top: "auto", bottom: 0 /* , backgroundColor: "#183650" */ }}
     >
-      <Toolbar className="tb22">
-        <Grid container display="flex" direction="row" alignItems="center">
+      <Toolbar
+        /* className="tb22" */ style={{ padding: 0, minHeight: 0 }}
+        id="test"
+      >
+        <Grid
+          container
+          display="flex"
+          direction="row"
+          alignItems="center"
+          style={{ padding: 0 }}
+        >
           <Grid item className="grid-item-a" xs={6} textAlign="center">
-            <a href="/users" style={{ width: "100%" }}>
-              Users
-            </a>
+            <Link to="/navbar">
+              <Button
+                id="nav-button"
+                color="secondary"
+                sx={{ width: "100%", color: "#F0F0F0" }}
+              >
+                Users
+              </Button>
+            </Link>
           </Grid>
           <Grid item xs={6} className="grid-item-b" textAlign="center">
-            <a href="/" style={{ width: "100%" }}>
-              Projects
-            </a>
+            <Link to="/navbar">
+              <Button id="nav-button" sx={{ width: "100%", color: "#F0F0F0" }}>
+                Projects
+              </Button>
+            </Link>
           </Grid>
         </Grid>
 
-        <StyledFab color="#787878" aria-label="add">
+        <StyledFab id="plus" aria-label="add" sx={boxSX}>
           +
         </StyledFab>
         <Box sx={{ flexGrow: 1 }} />
