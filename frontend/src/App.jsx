867eea65152c19310413db77663ba6747cb78624
PIERRE AHSATAL <asathal.pierre@gmail.com>
2022-06-30 11:02:40 +0200

    added login & register components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-
+import Register from "@components/Register";
+import Login from "@components/Login";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<div>DASHBOARD</div>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </Router>
       </Suspense>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./main.css";
 
 const rootContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(rootContainer);
