2a7da9a8be3ff24ffe900dba6077da7cdbb4e57c
puokl <phuoc@sapo.pt>
2022-06-30 13:53:02 +0200

    project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,13 @@ import ProjectsTable from "@components/ProjectsTable";
 import Register from "@components/Register/Register";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import ProjectCard from "@pages/ProjectCard/ProjectCard";
+import ProjectCard from "@pages/projectCard/ProjectCard";
 import ApplyToProjectModal from "@components/ApplyToProjectModal/ApplyToProjectModal";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
-import PageHeader from "@components/order/PageHeader";
+import PageHeader from "@components/PageLayout/PageHeader";
 
 // import projectsAtom from "@recoil/projects";
 
