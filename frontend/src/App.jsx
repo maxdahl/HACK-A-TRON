cbc9db886d40b6163a5ec732ab786fdeb2c5476f
Max Dahl <github@daagur.de>
2022-06-30 10:43:58 +0200

    basic filter functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 
+import ProjectsTable from "@components/ProjectsTable";
+
+import projectsAtom from "@recoil/projects";
+
 import "./App.css";
+import dummyData from "./dummyData.json";
+
+const initializeData = ({ set }) => {
+  set(projectsAtom, dummyData.projects);
+};
 
 function App() {
   return (
-    <RecoilRoot>
-      <Suspense>
-        <Router>
-          <Routes>
-            <Route path="/" element={<div>DASHBOARD</div>} />
-          </Routes>
-        </Router>
-      </Suspense>
+    <RecoilRoot initializeState={initializeData}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProjectsTable />} />
+        </Routes>
+      </Router>
     </RecoilRoot>
   );
 }
